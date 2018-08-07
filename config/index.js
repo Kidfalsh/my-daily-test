4a019b6ee9fdb82c5d93f282476945646bea800b
unknown <theflashdye@163.com>
2018-08-07 18:06:25 +0800

    注释配置文件

--- config/index.js
+++ config/index.js
@@ -6,10 +6,11 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
+    // 开发环境下面的配置
     // Paths
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static', //子目录，一般存放css,js,image等文件
+    assetsPublicPath: '/', //根目录
+    //可利用该属性解决跨域问题
     proxyTable: {
       '/api': {
         target: 'http://www.theflashdye.com', // 接口的域名
@@ -22,11 +23,14 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: '0.0.0.0', // can be overwritten by process.env.HOST
+    //地址
+    host: '0.0.0.0', // can be overwritten by process.env.HOST 
+    //端口号设置，端口号占用出现问题可以在此处修改
     port: 63322, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: true,
-    errorOverlay: true,
-    notifyOnErrors: true,
+    autoOpenBrowser: true, //是否在编译后(输入命令行 npm run dev )打开
+    errorOverlay: true, //浏览器错误提示
+    notifyOnErrors: true,//跨平台错误提示
+    //使用文件系统（file system ）获取文件改动的通知devServer.watchOptions  
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
     
@@ -35,24 +39,27 @@ module.exports = {
      */
 
     // https://webpack.js.org/configuration/devtool/#development
-    devtool: 'cheap-module-eval-source-map',
-
+    devtool: 'cheap-module-eval-source-map', //增加调试，该属性为原始的源代码（仅限行）不可在生产环境中使用
     // If you have problems debugging vue-files in devtools,
     // set this to false - it *may* help
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
-    cacheBusting: true,
+    cacheBusting: true, //使缓存失效
 
-    cssSourceMap: true
+    cssSourceMap: true 
+    //代码压缩后进行调bug定位非常困难于是引入sourcemap记录压缩前后的位置信息记录，
+    //当产生错误时直接定位到未压缩前的位置，将大大的方便我们调试
   },
 
   build: {
+    //生产环境下面的配置
     // Template for index.html
     index: path.resolve(__dirname, '../dist/index.html'),
-
+    ////index编译后生成的位置和名字，根据需要改变后缀，比如index.php
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsRoot: path.resolve(__dirname, '../dist'), //编译后存放生成环境代码的位置
+    assetsSubDirectory: 'static', //js,css,images存放文件夹名
+   
+    assetsPublicPath: '/', //发布的根目录，通常本地打包dist后打开文件会报错，此处修改为./。如果是上线的文件，可根据文件存放位置进行更改路径
 
     /**
      * Source Maps
@@ -66,6 +73,7 @@ module.exports = {
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
+    //unit的gzip命令用来压缩文件，gzip模式下需要压缩的文件的扩展名有js和css
     productionGzip: false,
     productionGzipExtensions: ['js', 'css'],
 
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,4 @@
+//当开发是调取dev.env.js的开发环境配置，发布时调用prod.env.js的生产环境配置
 'use strict'
 module.exports = {
   NODE_ENV: '"production"'
